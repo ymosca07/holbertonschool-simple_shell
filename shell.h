d13ddb99564b1388c684ead1e5fde1eec6eb5ee2
root <root@DESKTOP-A00A4R8.>
2025-01-04 01:21:23 +0100

    Ajout du shell.h : fichier header contenant les prototypes de fonctions et les includes necessaires

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+
+extern char **environ;
+char *_getenv(const char *name);
+char *find_command(const char *command);
+int read_input(char *args[]);
+void execute_command(char *args[]);
+
+#endif

aab63a78aae26cf002496c79c896e7281a985c83
root <root@DESKTOP-A00A4R8.>
2025-01-04 01:33:30 +0100

    Ajout du prototype absolute_command et main

--- shell.h
+++ shell.h
@@ -12,5 +12,7 @@ char *_getenv(const char *name);
 char *find_command(const char *command);
 int read_input(char *args[]);
 void execute_command(char *args[]);
+int main(void);
+char *absolute_command(const char *command);
 
 #endif

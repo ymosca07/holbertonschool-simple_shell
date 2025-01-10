d22682a9142a9363c255579d9379548e7001e0ed
Yannis <yannismosca@gmail.com>
2025-01-10 17:33:07 +0100

    condition pour le free

--- execute.c
+++ execute.c
@@ -45,5 +45,7 @@ void execute_command(char *args[], char *argv[])
 		while (args[i] != NULL)
 			free(args[i++]);
 	}
-	free(args);
+	
+	if (absolute_command(args[0]) != NULL)
+        free(full_path);
 }
